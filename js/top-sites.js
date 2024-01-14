1cf5883f4a212c142d9738c2a26c849b2064e976
shiny <vioulo@outlook.com>
2024-01-14 21:14:54 +0800

    修改常访问链接逻辑

--- js/top-sites.js
+++ js/top-sites.js
@@ -1,5 +1,10 @@
 browser.storage.local.get('otab_link_count').then((res) => {
     let rs = res['otab_link_count'] || [];
+    const el_link = document.querySelector('.view');
+    if (!rs.length) {
+        el_link.innerText = '// 没有任何浏览记录';
+        return;
+    }
 
     browser.bookmarks.get(rs.map(data => data.id)).then((bookmarks) => {
         const result = rs.map((data) => {
@@ -13,19 +18,15 @@ browser.storage.local.get('otab_link_count').then((res) => {
                 };
             }
         });
-
-        const el_link = document.querySelector('.view');
-        if (!result.length) {
-            el_link.innerText = '// 没有任何浏览记录';
-            return;
-        }
         // 使用数组的 sort() 方法按照 count 倒序排列
         result.sort((a, b) => b.count - a.count);
         for (let s of result.slice(0, 10)) {
             let a = document.createElement('a');
+            a.href = s.url;
             a.innerText = s.title;
             a.setAttribute('tb_id', s.id || 0);
-            a.onclick = () => {
+            a.onclick = (e) => {
+                e.preventDefault();
                 goLink(s.id, s.url);
             }
             el_link.appendChild(a);
