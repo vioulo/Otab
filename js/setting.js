9ec7e56de698b0d2746cc3be8b717b689f09be5b
shiny <vioulo@outlook.com>
2024-02-16 16:17:59 +0800

    添加自定义  css 设置

--- js/setting.js
+++ js/setting.js
@@ -1,10 +1,18 @@
-browser.storage.local.get('bar_pox', function (r) {
+browser.storage.sync.get('bar_pox', function (r) {
     let bar_pox = r.bar_pox || 'left';
     $(`.bar-${bar_pox}`).addClass('st-bar-act');
+});
+
+browser.storage.sync.get('otab-cus-css', function (r) {
+    $('#cus-css').val(r['otab-cus-css'] || '');
 });
 
 $('.bar-it').on('click', function () {
     $('.bar-it').removeClass('st-bar-act');
     $(this).addClass('st-bar-act');
-    browser.storage.local.set({ 'bar_pox': $(this).attr('pox') });
+    browser.storage.sync.set({ 'bar_pox': $(this).attr('pox') });
+});
+
+$('.cc-cfm').on('click', function () {
+    browser.storage.sync.set({ 'otab-cus-css': $('#cus-css').val() });
 });
