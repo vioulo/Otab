7d16ac26e9f4234e2cd4919b6be8c511c03c9155
shiny <vioulo@outlook.com>
2025-02-01 23:02:45 +0800

    将 new-tab 添加为主页

--- js/setting.js
+++ js/setting.js
@@ -1,16 +1,18 @@
+// 侧边栏位置
 browser.storage.sync.get('bar_pox', function (r) {
     let bar_pox = r.bar_pox || 'left';
     $(`.bar-${bar_pox}`).addClass('st-bar-act');
 });
 
-browser.storage.sync.get('otab-cus-css', function (r) {
-    $('#cus-css').val(r['otab-cus-css'] || '');
-});
-
-$('.bar-it').on('click', function () {
-    $('.bar-it').removeClass('st-bar-act');
+$('.st-bar .bar-it').on('click', function () {
+    $('.st-bar .bar-it').removeClass('st-bar-act');
     $(this).addClass('st-bar-act');
     browser.storage.sync.set({ 'bar_pox': $(this).attr('pox') });
+});
+
+// 自定义 CSS
+browser.storage.sync.get('otab-cus-css', function (r) {
+    $('#cus-css').val(r['otab-cus-css'] || '');
 });
 
 $('.cc-cfm').on('click', function () {
